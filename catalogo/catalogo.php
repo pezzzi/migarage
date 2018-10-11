798d7cce944c4d0f1ae633e0b4a8602a541fad54
GabrielChoi <gabychoi96@gmail.com>
2018-10-11 20:29:23 -0300

    cambios a catalogo

--- catalogo/catalogo.php
+++ catalogo/catalogo.php
@@ -1,20 +1,18 @@
-<pre>
 <?php
 
-$dsn = 'mysql:host=localhost;dbname=movies_db';
-$user = 'juancarlos';
-$pass = '123456';
+$dsn = 'mysql:host=localhost;dbname=migarage_db';
+$user = 'root';
+$pass = '';
 $opt= [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ];
 
 try{
 
     $conex = new PDO($dsn, $user, $pass, $opt);
 
-    $query = $conex->query('SELECT * FROM movies');
+    $query = $conex->query('SELECT * FROM publicaciones');
 
-    $peliculas = $query->fetchAll(PDO::FETCH_ASSOC);
+    $publicaciones = $query->fetchAll(PDO::FETCH_ASSOC);
 
-    var_dump($peliculas[0]['title']);
 
     // var_dump($conex);
 
@@ -35,19 +33,21 @@ try{
 <html lang="en" dir="ltr">
   <head>
     <link rel="stylesheet" href="../home/css/css/bootstrap.css">
-    <link rel="stylesheet" href="../home/css/styles.css">
     <link rel="stylesheet" href="css/scatalogo.css">
+    <link rel="stylesheet" href="../home/css/styles.css">
     <meta charset="utf-8">
     <title></title>
   </head>
   <body>
-    <?php include('../home/header.php') ?>
+    <header>
+      <?php include('../home/header.php') ?>
 
+    </header>
     <section>
       <article class="main">
-        <div class="row">
-          <div class="col-8">
-
+        <div class="row  yellow">
+          <div class="offset-2 col-9 black">
+            
           </div>
 
         </div>
