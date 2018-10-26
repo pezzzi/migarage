45a77c96381f009fcdf316ea3a444f1a4bee73bf
GabrielChoi <gabychoi96@gmail.com>
2018-10-25 21:55:31 -0300

    modificacion del home

--- home/home.php
+++ home/home.php
@@ -14,7 +14,7 @@
       <section>
         <article class="mainHome">
           <div class="row justify-content-center searchFilter marginRight">
-            <div class="col-5">
+            <div class="col-lg-5 col-md-10  col-xs-8">
               <h2 class="hHome">Busca un garage</h2>
               <form class="" action="../catalogo/catalogo.php" method="get">
                 <select class="homeMain" name="provincias">
@@ -33,25 +33,24 @@
 
                 <label class="navBarRadio">
                   <p class="formP">Tipo de vehiculo</p>
-                  <input type="radio" name="vehicle" value="auto">Auto
-                  <input type="radio" name="vehicle" value="camion">Camion
-                  <input type="radio" name="vehicle" value="camioneta">Camioneta
-                  <input type="radio" name="vehicle" value="moto">Moto
+                  <input type="radio" id="auto" name="vehicle" value="auto">
+                  <label for="auto">Auto</label>
+                  <input type="radio" id="camion" name="vehicle" value="camion">
+                  <label for="camion">Camion</label>
+                  <input type="radio" id="camioneta" name="vehicle" value="camioneta">
+                  <label for="camioneta">Camioneta</label>
+                  <input type="radio" id="moto" name="vehicle" value="moto">
+                  <label for="moto">Moto</label>
                 </label>
                 <br>
-                <p>Tiempo de estadia</p>
-                  <div class="col-2 centered">
-                     <input class="block" type="text" name="estadia" value="">
-                  </div>
-                    <div class="col-2 block">
-                      <select class="block" name="tiempo">
-                          <option value="hs">Hora</option>
-                          <option value="day">Dia</option>
-                          <option value="month">Mes</option>
-                      </select>
-                    </div>
+                <p class="formP">Estadia</p>
+                  <input type="radio" name="stay" value="hora">Hora
+                  <input type="radio" name="stay" value="dia">Dia
+                  <input type="radio" name="stay" value="mes">Mes
+                <br>
+                <button class="button" type="button" name="button">Hola</button>
                 </div>
-                <input class="floatL" type="submit" name="buscar" value="Buscar">
+
               </form>
             </div>
           </div>
@@ -61,6 +60,6 @@
     <footer>
       <?php include('footer.php') ?>
     </footer>
-
+<button class="button" type="button" name="button">pp</button>
   </body>
 </html>
