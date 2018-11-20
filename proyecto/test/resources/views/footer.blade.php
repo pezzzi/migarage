0c7678880ae1db266e3032d879ca70bfddb8554f
GabrielChoi <gabychoi96@gmail.com>
2018-11-20 11:39:06 -0300

    .

--- proyecto/test/resources/views/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="row justify-content-center marginRight">
-  <div class="col-8 col-md-9 col-lg-9">
-    <p>Mi Garage 2018™-Todos los derechos reservados</p
-  </div>
-</div>
-<script type="text/javascript" src="/js/jquery.js"></script>
-<script type="text/javascript" src="/js/bootstrap.js"></script>
--- proyecto/test/resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.default')
-
-@section('main')
-  @include('home')
-@endsection
